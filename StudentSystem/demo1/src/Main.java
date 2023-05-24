82083dc094f5093f2ee9cfb688a2699d4914d12f
Chenhanne <116365899+Chenhanne@users.noreply.github.com>
2023-05-24 15:57:33 +0800

    Delete StudentSystem directory

--- StudentSystem/demo1/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author ghr
- * @date ${YEAR}-${MONTH}-${DAY} ${TIME}
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
